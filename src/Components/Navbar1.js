1cf01461c9efd569b4e01e87c885bd7a96932cf4
owais8113 <127936539+owais8113@users.noreply.github.com>
2023-06-26 23:39:32 +0530

    Update Navbar1.js

--- src/Components/Navbar1.js
+++ src/Components/Navbar1.js
@@ -7,7 +7,16 @@ const Navbar = () => {
     <nav className="navbar1">
       <div className="navbar1-container">
         <Link to="/" className="navbar1-logo">
-          <p className='heading'>Owais <span>Ahmed</span></p>
+          <p className='heading'><span>HOME</span></p>
+        </Link>
+        <Link to="/project1" className="navbar1-logo">
+          <p className='heading'>Smart <span> <br/>Agriculture</span></p>
+        </Link>
+        <Link to="/project2" className="navbar1-logo">
+          <p className='heading'>Line <span> <br/>Follower</span></p>
+        </Link>
+        <Link to="/project3" className="navbar1-logo">
+          <p className='heading'>Smart <span> <br/>Attendance</span></p>
         </Link>
       </div>
     </nav>
